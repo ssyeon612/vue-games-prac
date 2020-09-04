adbd63d3d2ef3b08fdae314ea083a62afcf3875a
ssyeon612 <ssyeon612@gmail.com>
2020-09-04 17:46:39 +0900

    ADD: response check

---
+++ vue-webgame/3.숫자야구/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';      //package.json에서 설치한 vue를 가져와서 쓸 수 있다.
+import NumberBaseball from "./NumberBaseball.vue";
+
+new Vue().$mount('#root');
---
+++ vue-webgame/4.반응속도체크/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';      //package.json에서 설치한 vue를 가져와서 쓸 수 있다.
+import ResponseCheck from "./ResponseCheck.vue";
+
+new Vue().$mount('#root');
