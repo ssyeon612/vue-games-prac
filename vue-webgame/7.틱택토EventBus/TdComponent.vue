0416cf6485a9d1712e0964cb4ab4248a31768fee
ssyeon612 <ssyeon612@gmail.com>
2020-09-07 15:36:52 +0900

    ADD: tic tak toe eventbus

---
+++ vue-webgame/7.틱택토EventBus/TdComponent.vue
@@ -0,0 +1,20 @@
+ <template>
+    <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+    import EventBus from './EventBus';
+    export default {
+        props: {
+            cellData: String,
+            rowIndex: Number,
+            cellIndex: Number,
+        },
+        methods: {
+            onClickTd() {
+                if(this.cellData) return;           //누른 칸을 또 눌렀을 경우
+                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
+            },
+        }
+    };
+</script>
